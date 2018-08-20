0a87b2458d3e2839076ead87d3225aa481f1451d
Anuson Chanthasuk <khrutik@gmail.com>
2018-08-21 05:52:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>My First Website</title>
+</head>
+<body>
+   This is my very first website and I'm <b>extremely excited!!!!!!</b>
+   <h1>My First Website</h1>
+   <h2>An HTML Playground</h2>
+   <h3>An HTML Playground</h3>
+   <h4>An HTML Playground</h4>
+   <h5>An HTML Playground</h5>
+   <h6>An HTML Playground</h6>
+<p>
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+   Nullam facilisis arcu vel mollis finibus. Nunc facilisis 
+   vel nisl lacinia cursus. Cras suscipit augue sed volutpat 
+   tincidunt. Aenean dictum tincidunt urna, quis eleifend 
+   quam mattis eu. Integer sollicitudin, nisl faucibus aliquam 
+   ullamcorper, metus sapien scelerisque lorem, at ornare dui 
+   orci non orci. Integer tempus consectetur metus, vitae 
+   blandit nibh aliquam nec. Pellentesque vestibulum arcu eget 
+   ante sollicitudin, id accumsan dui molestie. Suspendisse 
+   vehicula semper dui id congue. Suspendisse sed velit sit 
+   amet velit luctus varius. Ut condimentum tincidunt consequat. 
+   Sed eu ligula non magna scelerisque auctor.
+</p>
+
+<b>Sed efficitur laoreet nisl,</b><br> 
+ <i>ac faucibus velit hendrerit sit amet.</i><br> 
+ <u>Phasellus ac orci eget nisi porta accumsan a eget ex.</u><br>
+ <em>Nam lacinia dolor at mi tristique rhoncus.</em><br>
+ <strong>Maecenas nisl est, rhoncus id cursus non, tempor a neque.</strong>
+
+<ul>
+   <li>apples
+      <ul>
+         <li>golden delicious</li>
+         <li>granny smith</li>
+         <li>gala</li>
+      </ul>
+   </li>
+   <li>oranges</li>
+   <li>pineapples</li>
+   <li>mangoes</li>
+   <li>dragonfruit</li>
+</ul>
+<table border="1" cellpadding="10" cellspacing="0">
+   <tr>
+      <th></th>
+      <th>Month</th>
+      <th>Rent</th>
+      <th colspan="2">Utilities</th><!-- This cell will span 2 columns in the row below it -->
+      <th>Groceries</th>
+      <th>Eating Out</th>
+      <th>Entertainment</th>
+   </tr>
+    <tr>
+      <td rowspan="3">Fall</td><!-- this cell will span 3 rows, June, July, & August -->
+      <td>June</td>
+      <td>$1500</td>
+      <td>$100</td><!-- The $100 and $50 cells will be under the Utilities cell-->
+      <td>$50</td>
+      <td>$350</td>
+      <td>$100</td>
+      <td>$50</td>
+   </tr>
+   <tr>
+      <td>July</td>
+      <td>$1500</td>
+      <td>$100</td>
+      <td>$50</td>
+      <td>$350</td>
+      <td>$100</td>
+      <td>$50</td>
+   </tr>
+   <tr>
+      <td>August</td>
+      <td>$1500</td>
+      <td>$100</td>
+      <td>$50</td>
+      <td>$350</td>
+      <td>$100</td>
+      <td>$50</td>
+   </tr>
+</table>
+</body>
+</html>
